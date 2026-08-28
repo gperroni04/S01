8f98f066b936c88edac2cc0f8853aa5ba5450307
gperroni04 <gabriel.amilton@yahoo.com>
2026-08-27 23:10:43 -0300

    Add number analysis functions in exercicio4.lua

---
+++ Relatorio-02/exercicio4.lua
@@ -0,0 +1,45 @@
+print("--- ANALISADOR DE NUMEROS ---")
+
+function calcularMedia(a, b)
+    return (a + b) / 2
+end
+
+function encontrarMaior(a, b)
+    if a > b then
+        return a
+    else
+        return b
+    end
+end
+
+function calcularDiferencaAbsoluta(a, b)
+    return math.abs(a - b)
+end
+
+function analisarNumeros(n1, n2, operacao)
+    if operacao == "media" then
+        return calcularMedia(n1, n2)
+
+    elseif operacao == "maior" then
+        return encontrarMaior(n1, n2)
+
+    elseif operacao == "diferenca" then
+        return calcularDiferencaAbsoluta(n1, n2)
+
+    else
+        return "Operação inválida!"
+    end
+end
+
+print("Digite o primeiro numero:")
+local n1 = tonumber(io.read())
+
+print("Digite o segundo numero:")
+local n2 = tonumber(io.read())
+
+print('Digite a operacao ("media", "maior" ou "diferenca"):')
+local operacao = io.read()
+
+local resultado = analisarNumeros(n1, n2, operacao)
+
+print("Resultado: " .. resultado)
